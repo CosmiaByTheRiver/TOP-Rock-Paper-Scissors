8c7d2684770d4a48892070c268e6ac5206c9a2f5
CosmiaByTheRiver <107163101+CosmiaByTheRiver@users.noreply.github.com>
2023-05-19 00:55:24 -0700

    Add 'game' function that fulfills project reqs

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
 let arrPick;
 
-            function getComputerChoice() {
+function getComputerChoice() {
 
-                arr = ["Rock", "Paper", "Scissors"];
+    arr = ["Rock", "Paper", "Scissors"];
 
-            randomized = Math.floor(Math.random() * arr.length);
+  randomized = Math.floor(Math.random() * arr.length);
 
-            arrPick = arr[randomized];
-                return arrPick;
+    arrPick = arr[randomized];
+    return arrPick;
 
 }
 
-getComputerChoice();
 
+//----------------------------------------//
 
 
 function oneRound(playerSelection, computerSelection) {
 
     playerSelection = playerSelection.toLowerCase(playerSelection);
 
-
+                //rock
     if (playerSelection == playerSelection && computerSelection == "Paper") {
-        return "You lost."
-    } else {
-        return "Invalid."
+        return "You lost. Paper beats rock."
+    }
+                //rock
+    else if (playerSelection == playerSelection && computerSelection == "Scissors") {
+        return "You win! Rock beats scissors.";
+    }
+              //paper
+    else if (playerSelection == playerSelection && computerSelection == "Rock") {
+        return "You win! Paper beats rock.";
+    }
+            //paper
+    else if (playerSelection == playerSelection && computerSelection == "Scissors") {
+        return "You lose. Scissors beats paper.";
+    }
+                //scissors
+    else if (playerSelection == playerSelection && computerSelection == "Paper") {
+        return "You win! Scissors beats paper.";
+    }
+              //scissors
+    else if (playerSelection == playerSelection && computerSelection == "Rock") {
+        return "You lose. Rock beats scissors.";
     }
 }
 
 let playerSelection  = "rock";
 let computerSelection = getComputerChoice();
 
-console.log(oneRound(playerSelection, computerSelection));
+
+// -------------------------------------------//
+
+
+function game() {
+    console.log(oneRound(playerSelection, getComputerChoice()));
+    console.log(oneRound(playerSelection, getComputerChoice()));
+    console.log(oneRound(playerSelection, getComputerChoice()));
+    console.log(oneRound(playerSelection, getComputerChoice()));
+    console.log(oneRound(playerSelection, getComputerChoice()));
+}
+
+game();
